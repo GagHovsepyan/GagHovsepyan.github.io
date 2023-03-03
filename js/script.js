15ca5d7234578a6914cea83d46e941af9afb80fd
GagHovsepyan <gagik.hovsepyan093095463@gmail.com>
2023-03-03 18:35:24 +0400

    sss

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// function myFunction(){
+// 	document.getElementById("logo").style.display = "none";
+//     document.getElementById("glavmenudiv").style.display = "block";
+//     document.getElementById("li1").style.display = "none";
+    
+// }
+// function closex(){
+// 	document.getElementById("glavmenudiv").style.display = "none";
+//     document.getElementById("logo").style.display = "block";
+// }
